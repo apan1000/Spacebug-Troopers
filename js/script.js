318cfcad822796fdd9ec7d2192394721611d1b69
Rodrigo Roa Rodríguez <rodrigoroarod@gmail.com>
2016-01-11 00:13:46 +0100

    Player collision and fixed mirroring bug

--- js/script.js
+++ js/script.js
@@ -1,13 +1,18 @@
-// WEBSPEECH API VARIABLES
 
+////////////////////////////////////////////////////////////////////////////////
+// VARIABLES
+////////////////////////////////////////////////////////////////////////////////
+
+// WEBSPEECH API VARIABLES
 var SpeechRecognition = SpeechRecognition || webkitSpeechRecognition
 var recognition = new SpeechRecognition();
 
+//webspeech setup
 recognition.continuous = true;
 recognition.lang = 'en_US';
 recognition.interimResults = true;
+recognition.onresult = OnVoiceRecognition;
 recognition.onend = function() {recognition.start();}
-
 
 // GAME VARIABLES
 var game = new Phaser.Game(700, 700, Phaser.CANVAS, '', { preload: preload, create: create, update: update });
@@ -24,8 +29,6 @@ var score = 0;
 var selectedPlayerText;
 var selectedPlayerText;
 
-
-// New variables
 var qKey;
 var explosion;
 var playerXPos;
@@ -33,12 +36,15 @@ var playerYPos;
 var SFX;
 
 var SCALE = 3;
-// New variables
 
 var colors = ['red', 'green', 'blue'];
 var monsterColors = [0x404040, 0xf0f000, 0xf050a0]; // Used for tinting
 var monsterNames = ['black', 'yellow', 'orange'];
 
+
+////////////////////////////////////////////////////////////////////////////////
+// Initialization
+////////////////////////////////////////////////////////////////////////////////
 
 function preload() {
 
@@ -65,10 +71,180 @@ function preload() {
 }
 
 
-// Get frames numbers for a row from a spritesheet.
+function create() {
+
+    // New create stuff
+    SFX = game.add.audio('sfx');
+    SFX.allowMultiple = true;
+
+
+    //  A simple background for our game
+    game.add.sprite(0, 0, 'chessmap');
+
+    //  Create our controls.
+    configureKeys();
+
+    createMonsters();
+    createSoldiers();
+
+    //  The select text
+    game.add.text(10, 5, 'Selected:', { font: '20px "Press Start 2P"', fill: '#000' });
+    selectedPlayerText = game.add.text(200, 5, 'red', { font: '20px "Press Start 2P"'});
+    // Set default selected player
+    selectPlayer('red')
+
+    // The text that counts the amount of moves
+    game.add.text(10, 35, 'Score:', { font: '20px "Press Start 2P"', fill: '#000' });
+
+    scoreText = game.add.text(140, 35, score, { font: '20px "Press Start 2P"', fill: '#FFF'});
+
+    // Start speech recognition
+    recognition.start();
+}
+
+function configureKeys() {
+      // Define keys
+      var key_ONE = game.input.keyboard.addKey(Phaser.Keyboard.ONE);
+      var key_TWO = game.input.keyboard.addKey(Phaser.Keyboard.TWO);
+      var key_THREE = game.input.keyboard.addKey(Phaser.Keyboard.THREE);
+      var key_W = game.input.keyboard.addKey(Phaser.Keyboard.W);
+      var key_A = game.input.keyboard.addKey(Phaser.Keyboard.A);
+      var key_S = game.input.keyboard.addKey(Phaser.Keyboard.S);
+      var key_D = game.input.keyboard.addKey(Phaser.Keyboard.D);
+      var key_ESC = game.input.keyboard.addKey(Phaser.Keyboard.ESC);
+      var cursors = game.input.keyboard.createCursorKeys();
+
+      qKey = game.input.keyboard.addKey(Phaser.Keyboard.Q);
+      eKey = game.input.keyboard.addKey(Phaser.Keyboard.E);
+
+      // Add callbacks to keys
+      // selection keys
+      key_ONE.onDown.add(function(){selectPlayer('red')}, this );
+      key_TWO.onDown.add(function(){selectPlayer('blue')}, this );
+      key_THREE.onDown.add(function(){selectPlayer('green')}, this );
+      // movement keys
+      key_W.onDown.add(function(){playerWalk('up')}, this );
+      key_A.onDown.add(function(){playerWalk('left')}, this );
+      key_S.onDown.add(function(){playerWalk('down')}, this );
+      key_D.onDown.add(function(){playerWalk('right')}, this );
+      cursors.up.onDown.add(function(){playerWalk('up')}, this );
+      cursors.left.onDown.add(function(){playerWalk('left')}, this );
+      cursors.down.onDown.add(function(){playerWalk('down')}, this );
+      cursors.right.onDown.add(function(){playerWalk('right')}, this );
+
+      // reset key
+      key_ESC.onDown.add(reset, this);
+}
+
+////////////////////////////////////////////////////////////////////////////////
+// METHODS
+////////////////////////////////////////////////////////////////////////////////
+
+// Gets frames numbers for a row from a spritesheet.
 function row(row, col){return _.range(col*row, col*row+col);}
 
-recognition.onresult = function(event) {
+// Special fuction for getting the player to walk
+function playerWalk(direction){
+  if(animationRunning) return; // Do nothing if an animation is still going
+  walk(player, direction);
+  monsterAction();
+  updateScore();
+}
+
+// Creates a new set of soldiers
+function createSoldiers() {
+
+    //Create soldier group
+    soldiers = game.add.group();
+    soldiers.enableBody = true;
+
+    //Add soldiers to group. Set anchor to middle so that character can be flipped without movement.
+
+    for (var i = 0; i < colors.length; i++) {
+      var soldier = soldiers.create(250 + i*100, 650, colors[i]+'soldier');
+      soldier.anchor.setTo(.5, .5);
+      soldier.scale.setTo(SCALE);
+    }
+
+    //Add animations to group
+    soldiers.callAll('animations.add', 'animations', 'walk_down', row(0, 4), 10, true);
+    soldiers.callAll('animations.add', 'animations', 'walk_up', row(1, 4), 10, true);
+    soldiers.callAll('animations.add', 'animations', 'walk_left', row(2, 4), 10, true);
+    animationRunning = false;
+}
+
+// Creates a new set of monsters
+function createMonsters() {
+
+    //Create monster group
+    monsters = game.add.group();
+    monsters.enableBody = true;
+
+    //Add monsters to group
+    for (var i = 0; i < 3; i++) {
+        var monster = monsters.create(250 + i*100, 350, 'monster');
+        monster.anchor.setTo(.5, .5);
+        monster.scale.setTo(SCALE);
+        monster.tint = monsterColors[i];
+        monster.name = monsterNames[i];
+    }
+
+    //Add animations to group. Play animation continously.
+    monsters.callAll('animations.add', 'animations', 'walk_down', row(0, 3), 10, true);
+    monsters.callAll('play', null, 'walk_down');
+}
+
+function walk (character, direction) {
+    var x = 0;
+    var y = 0;
+    var directionText = direction;
+
+    switch(direction){
+        case 'up': y = -100; break;
+        case 'down': y = 100; break;
+        case 'left':x = -100; break;
+        case 'right': x = 100;
+        direction = 'left'; // Since mirrored, play left animation.
+        break;
+    }
+
+    //Calculate new position
+    var newX = character.x + x;
+    var newY = character.y + y;
+
+    //Create a transition to the new location
+    if(newX < game.world.width && newX > 0 && newY < game.world.height && newY > 0) {
+      if(!soldierCollision(newX, newY)){
+        if(x==100) character.scale.setTo(-SCALE,SCALE); //mirror character
+        character.animations.play('walk_'+direction, 10, true);
+        animationRunning = true;
+        console.log(character.key+' is moving')
+        tween = this.game.add.tween(character).to({x:newX, y:newY}, 800, null, true);
+        tween.onComplete.addOnce(stopWalking, this);
+      }
+
+    }
+}
+
+function soldierCollision(newX, newY){
+  for (soldier of soldiers.children) {
+    console.log("x = "+newX+" y = "+newY);
+    console.log("x = "+soldier.x+" y = "+soldier.y);
+    if(soldier.x == newX && soldier.y == newY) return true;
+  }
+  //If here then there is no collision
+  console.log("No collision!")
+  return false;
+
+}
+
+
+////////////////////////////////////////////////////////////////////////////////
+// CALLBACKS
+////////////////////////////////////////////////////////////////////////////////
+
+// Gets called when something is said in the microphone.
+function OnVoiceRecognition(event) {
 
     var speechInput = '';
     var final_transcript = '';
@@ -105,163 +281,21 @@ recognition.onresult = function(event) {
 
 }
 
-function create() {
-
-    // New create stuff
-    SFX = game.add.audio('sfx');
-    SFX.allowMultiple = true;
-
-
-    //  A simple background for our game
-    game.add.sprite(0, 0, 'chessmap');
-
-    //  Create our controls.
-    configureKeys();
-
-    createMonsters();
-    createSoldiers();
-
-    //  The select text
-    game.add.text(10, 5, 'Selected:', { font: '20px "Press Start 2P"', fill: '#000' });
-    selectedPlayerText = game.add.text(200, 5, 'red', { font: '20px "Press Start 2P"'});
-    // Set default selected player
-    selectPlayer('red')
-
-    // The text that counts the amount of moves
-    game.add.text(10, 35, 'Score:', { font: '20px "Press Start 2P"', fill: '#000' });
-
-    scoreText = game.add.text(140, 35, score, { font: '20px "Press Start 2P"', fill: '#FFF'});
-
-    // Start speech recognition
-    recognition.start();
-}
-
+//Gets called every frame
 function update() {
   // Empty right now
-}
-function configureKeys() {
-      // Define keys
-      var key_ONE = game.input.keyboard.addKey(Phaser.Keyboard.ONE);
-      var key_TWO = game.input.keyboard.addKey(Phaser.Keyboard.TWO);
-      var key_THREE = game.input.keyboard.addKey(Phaser.Keyboard.THREE);
-      var key_W = game.input.keyboard.addKey(Phaser.Keyboard.W);
-      var key_A = game.input.keyboard.addKey(Phaser.Keyboard.A);
-      var key_S = game.input.keyboard.addKey(Phaser.Keyboard.S);
-      var key_D = game.input.keyboard.addKey(Phaser.Keyboard.D);
-      var key_ESC = game.input.keyboard.addKey(Phaser.Keyboard.ESC);
-      var cursors = game.input.keyboard.createCursorKeys();
-
-      qKey = game.input.keyboard.addKey(Phaser.Keyboard.Q);
-      eKey = game.input.keyboard.addKey(Phaser.Keyboard.E);
-
-      // Add callbacks to keys
-      // selection keys
-      key_ONE.onDown.add(function(){selectPlayer('red')}, this );
-      key_TWO.onDown.add(function(){selectPlayer('blue')}, this );
-      key_THREE.onDown.add(function(){selectPlayer('green')}, this );
-      // movement keys
-      key_W.onDown.add(function(){playerWalk('up')}, this );
-      key_A.onDown.add(function(){playerWalk('left')}, this );
-      key_S.onDown.add(function(){playerWalk('down')}, this );
-      key_D.onDown.add(function(){playerWalk('right')}, this );
-      cursors.up.onDown.add(function(){playerWalk('up')}, this );
-      cursors.left.onDown.add(function(){playerWalk('left')}, this );
-      cursors.down.onDown.add(function(){playerWalk('down')}, this );
-      cursors.right.onDown.add(function(){playerWalk('right')}, this );
-
-      // reset key
-      key_ESC.onDown.add(reset, this);
-
-}
-
-function playerWalk(direction){
-  if(animationRunning) return; // Do nothing if an animation is still going
-  walk(player, direction);
-  monsterAction();
-  updateScore();
-}
-
-function createSoldiers() {
-
-    //Create soldier group
-    soldiers = game.add.group();
-    soldiers.enableBody = true;
-
-    //Add soldiers to group. Set anchor to middle so that character can be flipped without movement.
-
-    for (var i = 0; i < colors.length; i++) {
-      var soldier = soldiers.create(250 + i*100, 650, colors[i]+'soldier');
-      soldier.anchor.setTo(.5, .5);
-      soldier.scale.setTo(SCALE);
-    }
-
-    //Add animations to group
-    soldiers.callAll('animations.add', 'animations', 'walk_down', row(0, 4), 10, true);
-    soldiers.callAll('animations.add', 'animations', 'walk_up', row(1, 4), 10, true);
-    soldiers.callAll('animations.add', 'animations', 'walk_left', row(2, 4), 10, true);
-    animationRunning = false;
-}
-
-function createMonsters() {
-
-    //Create monster group
-    monsters = game.add.group();
-    monsters.enableBody = true;
-
-    //Add monsters to group
-    for (var i = 0; i < 3; i++) {
-        var monster = monsters.create(250 + i*100, 350, 'monster');
-        monster.anchor.setTo(.5, .5);
-        monster.scale.setTo(SCALE);
-        monster.tint = monsterColors[i];
-        monster.name = monsterNames[i];
-    }
-
-    //Add animations to group. Play animation continously.
-    monsters.callAll('animations.add', 'animations', 'walk_down', row(0, 3), 10, true);
-    monsters.callAll('play', null, 'walk_down');
-}
-
-function walk (character, direction) {
-    var x = 0;
-    var y = 0;
-    var directionText = direction;
-
-    character.scale.setTo(SCALE,SCALE); //Unmirror character
-
-    switch(direction){
-        case 'up': y = -100; break;
-        case 'down': y = 100; break;
-        case 'left':x = -100; break;
-        case 'right': x = 100;
-        character.scale.setTo(-SCALE,SCALE); //mirror character
-        direction = 'left'; // Since mirrored, play left animation.
-        break;
-    }
-
-    //Calculate new position
-    var newX = character.x + x;
-    var newY = character.y + y;
-
-    //Create a transition to the new location
-    if(newX < game.world.width && newX > 0 && newY < game.world.height && newY > 0) {
-        character.animations.play('walk_'+direction, 10, true);
-        animationRunning = true;
-        console.log(character.key+' is moving')
-        tween = this.game.add.tween(character).to({x:newX, y:newY}, 800, null, true);
-        tween.onComplete.addOnce(stopWalking, this);
-    }
 }
 
 function stopWalking (character) {
     //Stop walking animations
-    animationRunning = false;
-    player.animations.stop('walk_left',true);
-    player.animations.stop('walk_up',true);
-    player.animations.stop('walk_down',true);
+    character.scale.setTo(SCALE,SCALE); //Unmirror character
+    player.animations.stop();
     character.frame = 0;
+    animationRunning = false;
     console.log(character.key+' is idle');
 }
+
+
 
 function walkToward (character, targetColor) {
     var characterXPos = character.position.x;
@@ -283,15 +317,13 @@ function walkToward (character, targetColor) {
             walk(character, 'up');
         }
     } else if ( Math.abs(targetXPos - characterXPos) > Math.abs(targetYPos - characterYPos) ) {
-        if (targetXPos > characterXPos) {
+        if (targetXPos > playerXPos) {
             walk(character, 'right');
         } else {
             walk(character, 'left');
         }
     }
 }
-
-
 
 function monsterAction () {
     playerXPos = player.position.x;
